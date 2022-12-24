6dbee5752499b124efcb9307ddc61f94a940113c
Sylvanus Prince <princehsylvanus@gmail.com>
2022-12-24 19:51:27 +0100

    electronic component created

---
+++ src/components/electronics-page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ElectronicsPage = () => {
+  return (
+    <div>
+        <h1>ELECTRONICS</h1>
+        <h1>ELECTRONICS</h1>
+        <h1>ELECTRONICS</h1>
+        <h1>ELECTRONICS</h1>
+        <h1>ELECTRONICS</h1>
+        <h1>ELECTRONICS</h1>
+        <h1>ELECTRONICS</h1>
+    </div>
+  )
+}
+
+export default ElectronicsPage
