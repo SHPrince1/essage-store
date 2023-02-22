bcaf8ff726712aa55dd9e99c12f96f76e27fc29e
Sylvanus Prince <princehsylvanus@gmail.com>
2023-02-22 16:53:47 +0100

    items learning

--- src/components/allshops.jsx
+++ src/components/allshops.jsx
@@ -18,12 +18,7 @@ import CardTv from "../components/card-tv";
 import Mencol from "../images/mencol.webp";
 import Smartwatch from "../images/smartwatch.webp";
 // import Men from "../images/mencol.webp";
-import Pef from "../images/pef.jpg";
-import Cream from "../images/cream.jpg";
-import Eye from "../images/eye1.jpg";
-import Hair from "../images/hair1.jpg";
-import Paste from "../images/paste.jpg";
-import Baby from "../images/baby.jpg";
+
 import Watch from "../images/watch.jpg";
 import Bag from "../images/bag.jpeg";
 import Pod from "../images/pod.jpg";
@@ -34,17 +29,12 @@ import Tv from "../images/tv1.webp";
 // import Tv1 from "../images/tv.webp";
 import Media from "../images/homet.webp";
 import Laptop from "../images/laptop.jpg";
-import Ps5 from '../images/ps5.webp';
-
-
-
+import Ps5 from "../images/ps5.webp";
 
 const AllShops = () => {
-
-  
-    const [productData, setProductData] = useState([]);
-    const [categoriesData, setCategoriesData] = useState([]);
-
+  const [productData, setProductData] = useState([]);
+  const [categoriesData, setCategoriesData] = useState([]);
+  const [fourLimitData, setFourLimitData] = useState([]);
 
   const options = {
     method: "GET",
@@ -57,7 +47,7 @@ const AllShops = () => {
       .then(function (response) {
         setProductData(response.data);
 
-        console.log (response.data);
+        console.log(response.data);
       })
       .catch(function (error) {
         console.error(error);
@@ -66,7 +56,7 @@ const AllShops = () => {
 
   const secOptions = {
     method: "GET",
-    url: 'https://fakestoreapi.com/products/category/jewelery',
+    url: "https://fakestoreapi.com/products/category/jewelery",
   };
 
   useEffect(() => {
@@ -75,13 +65,29 @@ const AllShops = () => {
       .then(function (response) {
         setCategoriesData(response.data);
 
-        console.log (response.data);
+        console.log(response.data);
       })
       .catch(function (error) {
         console.error(error);
       });
   });
+  const thirdOptions = {
+    method: "GET",
+    url: 'https://fakestoreapi.com/products?limit=8',
+  };
 
+  useEffect(() => {
+    axios
+      .request(thirdOptions)
+      .then(function (response) {
+        setFourLimitData(response.data);
+
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  });
 
   return (
     <div>
@@ -93,58 +99,41 @@ const AllShops = () => {
             <a href="https://essage.com">View All</a>
           </div>
           <div className={style1.cardBox}>
-
-            {productData.map((item, index)=>{
-              return(
-                <Cards key={item.id}
-                   title={item.title}
-                   img={item.image}
-                   price={item.price}
+            {productData.map((item, index) => {
+              return (
+                <Cards
+                  key={item.id}
+                  title={item.title}
+                  img={item.image}
+                  price={item.price}
                   //  rate={item.rating}
 
-                   category={item.category}
-                
+                  category={item.category}
                 />
-                
-              )
+              );
             })}
-           
 
             {}
           </div>
           {/* mobile view */}
-         
+
           <div className={style1.imgBanner}>
-            <img className={style1.imgCont} src={Mencol}  alt=""/>
+            <img className={style1.imgCont} src={Mencol} alt="" />
           </div>
           <div>
             <div className={style.textHead}>
               <p>DAILY PICKS</p>
-              <a href="href=https://essage.com" >View All</a>
+              <a href="href=https://essage.com">View All</a>
             </div>
             <div className={style1.cardBox}>
-              {categoriesData.map((item, index)=>
-              <CardE key={item.id} img={item.image}  title={item.title} price={item.price}/>
-              )}
-             
-            </div>
-          </div>
-
-          {/* mobile view */}
-          <div>
-            <div className={style1.mobilecardBox}>
-              <div className={style1.singleBox}>
-                <CardE title="skin care" price="10,000.00" img={Cream}alt="" />
-                <CardE title="Perfumes" price="20,000.00" img={Pef} alt="" />
-              </div>
-              <div className={style1.singleBox}>
-                <CardE title="Sun glass" price="5,720.000" img={Eye} alt="" />
-                <CardE title="Hair Care" price="3,000.00" img={Hair} alt=""/>
-              </div>
-              <div className={style1.singleBox}>
-                <CardE title="Baby lotion" price="15,000.00" img={Baby}alt="" />
-                <CardE title="tootpaste" price="1500.00" img={Paste}alt="" />
-              </div>
+              {categoriesData.map((item, index) => (
+                <CardE
+                  key={item.id}
+                  img={item.image}
+                  title={item.title}
+                  price={item.price}
+                />
+              ))}
             </div>
           </div>
 
@@ -158,12 +147,26 @@ const AllShops = () => {
             </div>
 
             <div className={style1.cardBox}>
-              <CardE title="Smart Watch" img={Watch} alt="" />
+              {/* <CardE title="Smart Watch" img={Watch} alt="" />
               <CardE title="Laptop Bag" img={Bag} alt="" />
               <CardE title="Ear Pod" img={Pod} alt="" />
               <CardE title="Wrist watch" img={Awatch} alt="" />
               <CardE title="Power Bank" img={Pbank} alt="" />
-              <CardE title="Bluetooth Speaker" img={Speaker} alt="" />
+              <CardE title="Bluetooth Speaker" img={Speaker} alt="" /> */}
+
+              {fourLimitData.map((item, index) => {
+                return (
+                  <Cards
+                    key={item.id}
+                    title={item.title}
+                    img={item.image}
+                    price={item.price}
+                    //  rate={item.rating}
+
+                    category={item.category}
+                  />
+                );
+              })}
             </div>
           </div>
           {/* mobile view */}
@@ -178,8 +181,8 @@ const AllShops = () => {
                 <CardE title="Wrist watch" img={Awatch} alt="" />
               </div>
               <div className={style1.singleBox}>
-                <CardE title="Power Bank" img={Pbank} alt=""/>
-                <CardE title="Bluetooth Speaker" img={Speaker} alt=""/>
+                <CardE title="Power Bank" img={Pbank} alt="" />
+                <CardE title="Bluetooth Speaker" img={Speaker} alt="" />
               </div>
             </div>
           </div>
@@ -190,22 +193,26 @@ const AllShops = () => {
           </div>
           <div className={style1.cardBox}>
             <CardTv
-              img={Tv} alt=""
+              img={Tv}
+              alt=""
               h6="A NNEMY TELEVISION GROUP"
               content="A NNEMY TELEVISION GROUP"
             />
             <CardTv
-              img={Media} alt=""
+              img={Media}
+              alt=""
               h6="A NNEMY TELEVISION GROUP"
               content="A NNEMY TELEVISION GROUP"
             />
             <CardTv
-              img={Ps5} alt=""
+              img={Ps5}
+              alt=""
               h6="A NNEMY TELEVISION GROUP"
               content="A NNEMY TELEVISION GROUP"
             />
             <CardTv
-              img={Laptop} alt=""
+              img={Laptop}
+              alt=""
               h6="A NNEMY TELEVISION GROUP"
               content="A NNEMY TELEVISION GROUP"
             />
@@ -216,22 +223,26 @@ const AllShops = () => {
             <div className={style1.mobilecardBox}>
               <div className={style1.singleBoxTv}>
                 <CardTv
-                  img={Tv} alt=""
+                  img={Tv}
+                  alt=""
                   h6="A NNEMY TELEVISION GROUP"
                   content="A NNEMY TELEVISION GROUP"
                 />
                 <CardTv
-                  img={Media} alt=""
+                  img={Media}
+                  alt=""
                   h6="A NNEMY TELEVISION GROUP"
                   content="A NNEMY TELEVISION GROUP"
                 />
                 <CardTv
-                  img={Ps5} alt=""
+                  img={Ps5}
+                  alt=""
                   h6="A NNEMY TELEVISION GROUP"
                   content="A NNEMY TELEVISION GROUP"
                 />
                 <CardTv
-                  img={Laptop} alt=""
+                  img={Laptop}
+                  alt=""
                   h6="A NNEMY TELEVISION GROUP"
                   content="A NNEMY TELEVISION GROUP"
                 />
@@ -240,8 +251,6 @@ const AllShops = () => {
           </div>
         </div>
       </Container>
-
-      
     </div>
   );
 };
